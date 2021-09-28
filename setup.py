b4e468b170a1ed0bd731ebdf4c980d385d2307f6
George Rahul <75750164+georgerahul24@users.noreply.github.com>
2021-09-28 16:08:47 +0530

    v0.0.5
    Ability to add fonts and font sizes
    Customize colours of individual buttons

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='tkinterstylize',
-    version='0.0.4',
+    version='0.0.5',
     packages=['tkinterstylize'],
     url='https://github.com/georgerahul24/tkinterstylize',
     license='MIT',
